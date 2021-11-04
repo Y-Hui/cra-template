2f9bb3028cd675454b561fa47362bfd8e0155d06
Y-Hui <y-hui.mail@foxmail.com>
2021-11-04 11:15:03 +0800

    🔧 build: 新增 tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+import reactJsx from 'vite-react-jsx'
+
+function pathResolve(dir: string) {
+  return resolve(__dirname, '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), svgr()],
+  resolve: {
+    alias: [
+      {
+        find: /@\//,
+        replacement: `${pathResolve('src')}/`,
+      },
+    ],
+  },
+  plugins: [reactRefresh(), reactJsx(), svgr()],
 })
